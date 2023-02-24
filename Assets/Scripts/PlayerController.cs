a4578f62cb50a689b53743db11ce3261d68a7589
Andreas Metz <sqrlycode@gmail.com>
2023-02-24 19:58:57 +0100

    Implemented ShuffleInput

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,9 +13,13 @@ public class PlayerController : MonoBehaviour
 
     private float _shootTimer;
     private Rigidbody2D _rb;
+    private bool _isRunning;
 
-    private Action<Vector2> _leftClickAction;
-    private Action<Vector2> _rightClickAction;
+    private Action _leftClickAction;
+    private Action _rightClickAction;
+    private Action _leftShiftAction;
+    
+    private Vector2 _mouseWorldPos;
     
     private void Awake()
     {
@@ -21,6 +27,7 @@ public class PlayerController : MonoBehaviour
 
         _leftClickAction = ShootAtPosition;
         _rightClickAction = TeleportToPosition;
+        _leftShiftAction = ToggleRunning;
     }
 
     void Update()
@@ -30,38 +37,70 @@ public class PlayerController : MonoBehaviour
         float vertInput = Input.GetAxis("Vertical");
         Vector2 moveInput = new Vector2(horInput, vertInput);
         _rb.velocity = moveInput * _moveSpeed;
+        if (_isRunning)
+            _rb.velocity *= 2f;
 
         //Rotate towards mouse
-        Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 dir = mouseWorldPos - _rb.position;
+        _mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 dir = _mouseWorldPos - _rb.position;
         transform.up = dir;
 
         //LeftClick
         if (Input.GetKeyDown(KeyCode.Mouse0))
-            _leftClickAction(mouseWorldPos);
+            _leftClickAction();
         
         //Rightclick
         if (Input.GetKeyDown(KeyCode.Mouse1))
-            _rightClickAction(mouseWorldPos);
+            _rightClickAction();
         
+        //Shiftclick
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+            _leftShiftAction();
         
         _shootTimer += Time.deltaTime;
     }
-    
-    
-    public void ShootAtPosition(Vector2 position)
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "InputShuffle")
+        {
+            Destroy(other.gameObject);
+            ShuffleInput();
+        }
+    }
+
+
+    public void ShootAtPosition()
     {
         if (_shootTimer > _shootCooldown)
         {
             _shootTimer = 0;
             Vector2 shootOrigin = (Vector2)transform.position + (Vector2)transform.up * 0.5f;
             GameObject newProjectileGo = Instantiate(_projectileGo, shootOrigin, Quaternion.identity);
-            newProjectileGo.transform.up = position - (Vector2)transform.position;
+            newProjectileGo.transform.up = _mouseWorldPos - (Vector2)transform.position;
         }
     }
 
-    private void TeleportToPosition(Vector2 position)
+    private void TeleportToPosition()
     {
-        transform.position = position;
+        transform.position = _mouseWorldPos;
+    }
+
+    private void ToggleRunning()
+    {
+        _isRunning = !_isRunning;
+    }
+
+    private void ShuffleInput()
+    {
+        List<Action> actionList = new List<Action>();
+        actionList.Add(ShootAtPosition);
+        actionList.Add(TeleportToPosition);
+        actionList.Add(ToggleRunning);
+        List<Action> randomizedActionList = actionList.OrderBy((x) => Random.Range(0, 2)).ToList();
+
+        _leftClickAction = randomizedActionList[0];
+        _rightClickAction = randomizedActionList[1];
+        _leftShiftAction = randomizedActionList[2];
     }
 }
