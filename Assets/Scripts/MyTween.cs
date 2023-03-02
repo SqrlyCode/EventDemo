c17fe6b623f4710e576d48bb45747c9010d1b024
Squirrel <metz@spengergasse.at>
2023-03-02 13:58:39 +0100

    Removed  already finished exercise from MyTween

--- Assets/Scripts/MyTween.cs
+++ Assets/Scripts/MyTween.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MyTween : MonoBehaviour
 {
-    public IEnumerator AnimatedScaleCR(Vector3 targetScale, float duration, Action onComplete = null)
+    public IEnumerator AnimatedScaleCR(Vector3 targetScale, float duration)
     {
         Vector3 initScale = transform.localScale;
         float timer = 0;
@@ -14,7 +14,5 @@ public class MyTween : MonoBehaviour
             transform.localScale = Vector3.Lerp(initScale, targetScale, timer / duration);
             yield return null;
         }
-
-        onComplete?.Invoke();
     }
 }
